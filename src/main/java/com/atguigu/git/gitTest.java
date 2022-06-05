f97e234c75a7af0939cf0999ffb7e5cf1b1356e8
JJJi <461939640@qq.com>
2022-06-05 21:09:16 +0800

    master push commit1

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -7,5 +7,6 @@ public class gitTest {
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("hot-fix");
+        System.out.println("push-test");
     }
 }
