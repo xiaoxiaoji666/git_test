4ad354567abcc5255c6714829465572df7f18397
xiaoxiaoji666 <72901191+xiaoxiaoji666@users.noreply.github.com>
2022-06-05 22:27:05 +0800

    Update gitTest.java

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -8,5 +8,6 @@ public class gitTest {
         System.out.println("hello git4");
         System.out.println("hot-fix");
         System.out.println("push-test");
+        System.out.println("pull-test");
     }
 }
